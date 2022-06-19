c7a137e364a5f5eb290bcc6d2c131c5b3b361a24
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-06-19 13:24:00 +0000

    [Update]4-25

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,11 @@
 class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :name, presence: true
+  
   def get_image
     if image.attached?
       image
