836924fac007bcd7ddfe24e0096cee419f7f4f1c
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-06-21 00:34:58 +0000

    [Update]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,11 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
   end
 
   def update
@@ -34,7 +39,7 @@ class BooksController < ApplicationController
       flash[:notice] = 'You have updated book successfully.'
       redirect_to book_path(@book.id)
     else
-      render :edit
+      render :books_path
     end
   end
 
